ccb9ec18c557850a1ab5b8232b5caccad4ef597c
OG4rcia <astra10071@gmail.com>
2023-05-23 23:30:20 -0300

    personalização login e cadastro

--- BitLandAPI/wwwroot/js/script_cadastro.js
+++ BitLandAPI/wwwroot/js/script_cadastro.js
@@ -1,22 +1,4 @@
-﻿function validarLogin() {
-    // Obter os valores dos campos de nome de usuário e senha
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-    console.log(username);
-    console.log(password);
-
-    // Validação simples (exemplo)
-    if (username && password) {
-        // Login válido, redirecionar para a página de destino
-        window.location.href = '../pages/app.html';
-    } else {
-        // Exibir mensagem de erro
-        alert('Nome de usuário ou senha inválidos!');
-    }
-}
-
-function redirecionarPagina() {
+﻿function redirecionarPagina() {
     // Redirecionar para a página de destino
     window.location.href = './index.html';
 }
--- BitLandAPI/wwwroot/js/script_login_bitland.js
+++ BitLandAPI/wwwroot/js/script_login_bitland.js
@@ -1,9 +1,49 @@
 dataTest = {};
 
-async function fazerLogin() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+$("#formlogin").find('input, textarea').on('keyup blur focus', function (e) {
 
+    var $this = $(this),
+        label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        } else {
+            label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+        if( $this.val() === '' ) {
+            label.removeClass('active highlight');
+        } else {
+            label.removeClass('highlight');
+        }
+    } else if (e.type === 'focus') {
+
+        if( $this.val() === '' ) {
+            label.removeClass('highlight');
+        }
+        else if( $this.val() !== '' ) {
+            label.addClass('highlight');
+        }
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+
+    e.preventDefault();
+
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+
+    target = $(this).attr('href');
+
+    $('.tab-content > div').not(target).hide();
+
+    $(target).fadeIn(600);
+
+});
+async function fazerLogin(username, password) {
     var data = {
         "login": username,
         "password": password,
@@ -36,6 +76,43 @@ async function fazerLogin() {
     }
 }
 
+function validarLogin() {
+    event.preventDefault()
+    // Obter os valores dos campos de nome de usuário e senha
+    var username = document.getElementById('login__username').value;
+    var password = document.getElementById('login__password').value;
+
+    console.log(username);
+    console.log(password);
+
+    // Validação simples (exemplo)
+    if (username && password) {
+        // Login válido, redirecionar para a página de destinofunction validarLogin() {
+        //     // Obter os valores dos campos de nome de usuário e senha
+        //     var username = document.getElementById('username').value;
+        //     var password = document.getElementById('password').value;
+        // 
+        //     console.log(username);
+        //     console.log(password);
+        // 
+        //     // Validação simples (exemplo)
+        //     if (username && password) {
+        //         // Login válido, redirecionar para a página de destino
+        //         window.location.href = '../pages/app.html';
+        //     } else {
+        //         // Exibir mensagem de erro
+        //         alert('Nome de usuário ou senha inválidos!');
+        //     }
+        // }
+        fazerLogin(username, password);
+        
+    } else {
+        // Exibir mensagem de erro
+        alert('Nome de usuário ou senha não preenchidos!');
+    }
+}
+
 function redirecionarPaginaLogin() {
     window.location.href = '../index.html';
 }
+
