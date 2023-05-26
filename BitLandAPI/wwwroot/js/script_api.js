d871fa3aacfa7aa6c1786b2f29beba096f55a593
DESKTOP-HA72MGQ\muril <murilodesouzabarros@gmail.com>
2023-05-25 23:04:16 -0300

    Alterações na estilização e exibição dos produtos filtrados.

--- BitLandAPI/wwwroot/js/script_api.js
+++ BitLandAPI/wwwroot/js/script_api.js
@@ -1,20 +1,20 @@
-var produtos = [];
-var produtospromocao = [];
+let produtos = [];
+let produtospromocao = [];
 
 const loadPagina = async () => {
     await checkJwt();
     checkLogin();
-    await getProducts();
+    await getProductsDestaque();
     // getProductsPromocao();
 }
 
 async function checkLogin() {
     if (localStorage.getItem("userjwt") != null)
     {
-        var response = await fetch("/clientes/" + localStorage.getItem("ClienteId"));
+        let response = await fetch("/clientes/" + localStorage.getItem("ClienteId"));
         const data = await response.json();
         user = JSON.parse(JSON.stringify(data));
-        var loginbutton = document.querySelector("#userInfo");
+        let loginbutton = document.querySelector("#userInfo");
         loginbutton.href = "";
     }
     else
@@ -31,7 +31,7 @@ const checkJwt = async () => {
     }
 }
 
-const getProducts = async () => {
+const getProductsDestaque = async () => {
     const response = await fetch("/produtos/destaques", {
         headers: {
             "ngrok-skip-browser-warning": "any"
@@ -45,19 +45,19 @@ const getProducts = async () => {
 function atualizarProdutos() {
     document.querySelector("#Grid").innerHTML = "";
     produtos.forEach(a => {
-        var liProduto = document.createElement("li");
-        liProduto.className = "itens"
-        var aLinkProduto = document.createElement("a");
+        let liProduto = document.createElement("li");
+        liProduto.className = "itens";
+        let aLinkProduto = document.createElement("a");
         aLinkProduto.href = "";
         aLinkProduto.className = "aTagProduto";
-        var imgProduto = document.createElement("img");
+        let imgProduto = document.createElement("img");
         imgProduto.src = window.location.origin + a.pathImage;
         imgProduto.alt = a.descricao;
         imgProduto.className = 'imgProduto';
-        var h3NomeProduto = document.createElement("h3");
+        let h3NomeProduto = document.createElement("h3");
         h3NomeProduto.innerText = a.nome;
         h3NomeProduto.className = 'nomeProduto';
-        var pPreco = document.createElement("p");
+        let pPreco = document.createElement("p");
         pPreco.className = "price";
         pPreco.innerText = (a.preco - (a.promocao / 100 * a.preco)).toFixed(2);
 
@@ -67,6 +67,37 @@ function atualizarProdutos() {
         liProduto.appendChild(aLinkProduto);
         document.querySelector("#Grid").appendChild(liProduto);
     });
+}
+
+function atualizarProdutosPromocao() {
+    document.querySelector("#Grid").innerHTML = "";
+    produtospromocao.forEach(a => {
+        let liProdutoPromocao = document.createElement("li");
+        liProdutoPromocao.className = "itens";
+        let aLinkProdutoPromocao = document.createElement("a");
+        aLinkProdutoPromocao.href = "#";
+        aLinkProdutoPromocao.className = "aTagProduto";
+        let imgProduto = document.createElement("img");
+        imgProduto.src = window.location.origin + a.pathImage;
+        imgProduto.alt = a.descricao;
+        imgProduto.className = 'imgProduto';
+        let h3NomeProdutoPromocao = document.createElement("h3");
+        h3NomeProdutoPromocao.innerText = a.nome;
+        h3NomeProdutoPromocao.className = 'nomeProduto';
+        let pPrecoNormal = document.createElement("p");
+        pPrecoNormal.className = "price-old";
+        pPrecoNormal.innerText = a.preco;
+        let pPrecoDesconto = document.createElement("p");
+        pPrecoDesconto.className = "price-new";
+        pPrecoDesconto.innerText = (a.preco - (a.promocao / 100 * a.preco)).toFixed(2);
+
+        aLinkProdutoPromocao.appendChild(imgProduto);
+        aLinkProdutoPromocao.appendChild(h3NomeProdutoPromocao);
+        aLinkProdutoPromocao.appendChild(pPrecoNormal);
+        aLinkProdutoPromocao.appendChild(pPrecoDesconto);
+        liProdutoPromocao.appendChild(aLinkProdutoPromocao);
+        document.querySelector("#Grid").appendChild(liProdutoPromocao);
+    })
 }
 
 const getProductsPromocao = async () => {
@@ -80,33 +111,6 @@ const getProductsPromocao = async () => {
     atualizarProdutosPromocao();
 }
 
-function atualizarProdutosPromocao() {
-    produtospromocao.forEach(a => {
-        var liProdutoPromocao = document.createElement("li");
-        var aLinkProdutoPromocao = document.createElement("a");
-        aLinkProdutoPromocao.href = "#";
-        var imgProduto = document.createElement("img");
-        imgProduto.src = window.location.origin + a.pathImage;
-        imgProduto.alt = a.descricao;
-        imgProduto.className = 'imageDestaque';
-        var h3NomeProdutoPromocao = document.createElement("h3");
-        h3NomeProdutoPromocao.innerText = a.nome;
-        var pPrecoNormal = document.createElement("p");
-        pPrecoNormal.className = "price-old";
-        pPrecoNormal.innerText = a.preco;
-        var pPrecoDesconto = document.createElement("p");
-        pPrecoDesconto.className = "price-new";
-        pPrecoDesconto.innerText = (a.preco - (a.promocao / 100 * a.preco)).toFixed(2);
-
-        aLinkProdutoPromocao.appendChild(imgProduto);
-        aLinkProdutoPromocao.appendChild(h3NomeProdutoPromocao);
-        aLinkProdutoPromocao.appendChild(pPrecoNormal);
-        aLinkProdutoPromocao.appendChild(pPrecoDesconto);
-        liProdutoPromocao.appendChild(aLinkProdutoPromocao);
-        document.querySelector("body > div.container > main > section.promotions > ul").appendChild(liProdutoPromocao);
-    })
-}
-
 async function filtrarCategoria(categoriaNum){
     const response = await fetch(window.location.origin + "/produtos/categoria/" + categoriaNum, {
         headers: {
@@ -115,6 +119,17 @@ async function filtrarCategoria(categoriaNum){
     });
     const data = await response.json();
     produtos = JSON.parse(JSON.stringify(data));
+    atualizarProdutos();
+}
+
+async function getTodosProdutos() {
+    const response = await fetch(window.location.origin + "/produtos", {
+        headers: {
+            
+        }
+    });
+    const data = await response.json();
+    produtos = JSON.parse(JSON.stringify(data))
     atualizarProdutos();
 }
 
